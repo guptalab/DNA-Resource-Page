e082b0af76a193937bbcaab35d93b295fe58eee8
Gupta Lab <mankg@guptalab.org>
2023-12-05 19:57:36 -0500

    Add final set of src files

---
+++ src/utils/authHelper.js
@@ -0,0 +1,10 @@
+
+// TODO: Add your sexy admins email here
+const sexyAdminsEmail = [
+    'neelporiya1309@gmail.com',
+    'mankg@guptalab.com'
+];
+
+const isUser_A_SexyAdmin = (email) => {
+    return sexyAdminsEmail.includes(email);
+};
